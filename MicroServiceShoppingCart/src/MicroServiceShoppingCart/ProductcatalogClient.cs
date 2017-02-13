ddc71c6669c6fdc8a4e20a460eb7fd92fdaff8dd
Arthur Gorr <art.gorr@gmail.com>
2017-02-13 12:51:50 -0600

    Partial impls, compiles but terribly inadequte

---
+++ MicroServiceShoppingCart/src/MicroServiceShoppingCart/ProductcatalogClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroServiceShoppingCart
+{
+    public interface IProductCatalogClient
+    {
+        Task<int[]> GetShoppingCartItems(int[] shoppingCartItems);
+    }
+
+    public class ProductCatalogClient : IProductCatalogClient
+    {
+        public async Task<int[]> GetShoppingCartItems(int[] shoppingCartItems)
+        {
+            return Enumerable.Empty<int>().ToArray();
+        }
+
+        private static async Task<HttpResponseMessage> RequestProductFromProductCatalogue(int[] productCatalogueIds)
+        {
+            var getProductPathTemplate = "/products?productIds=[{0}]";
+            var productsResource = string.Format(getProductPathTemplate, string.Join(",", productCatalogueIds));
+            using (var httpClient = new HttpClient())
+            {
+                var productCatalogBaseUrl = @"http://private-05cc8-chapter2productcatalogmicroservice.apiary-mock.com";
+                httpClient.BaseAddress = new Uri(productCatalogBaseUrl);
+                return await
+                httpClient.GetAsync(productsResource).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ MicroServiceShoppingCart/src/MicroServiceShoppingCart/ShoppingCartStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MicroServiceShoppingCart
+{
+    public class ShoppingCart
+    {
+        public void AddItems(int[] shoppingCartItems, IEventStore eventStore)
+        {
+
+        }
+
+        public void RemoveItems(int[] shoppingCartItems, IEventStore eventStore)
+        {
+
+        }
+    }
+
+    public interface IShoppingCartStore
+    {
+        ShoppingCart Get(int userId);
+        void Save(ShoppingCart shoppingCart);
+    }
+
+    public class ShoppingCartStore : IShoppingCartStore
+    {
+        public ShoppingCart Get(int userId)
+        {
+            return new ShoppingCart();
+        }
+
+        public void Save(ShoppingCart shoppingCart)
+        {
+        }
+    }
+}
